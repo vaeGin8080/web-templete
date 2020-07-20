209b76646e23870615266a885640dc17b89bca89
vaeGin8080 <1379202781@qq.com>
2020-07-20 14:40:44 +0800

    update: 修改公共组件

--- src/utils/components.js
+++ src/utils/components.js
@@ -1,18 +1,6 @@
 // 布局容器
 import Wrap from "@/components/Wrap";
 
-// 小型tab
-import Nav from "@/components/Nav";
-
-// 右侧aside
-import Aside from "@/components/Aside";
-
-// banner img
-import BGimg from "@/components/BGimg";
-
 export default {
   Wrap: Wrap,
-  Nav: Nav,
-  Aside: Aside,
-  BGimg: BGimg,
 };
